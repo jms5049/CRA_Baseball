b1cd7d29448c7a5ebd3ccee219385318512f2465
minsung.joh <minsung.joh@samsung.com>
2024-05-30 14:34:27 +0900

    [feature] input length

--- Sample-Test1/Baseball_Test.cpp
+++ Sample-Test1/Baseball_Test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
+#include "../Baseball/Baseball.cpp"
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExpectionWhenInputLemngthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
