970310caa89d2774bb49be977362ab24251e1f6e
minsung.joh <minsung.joh@samsung.com>
2024-05-30 15:42:25 +0900

    [refator] baseball test

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -1,24 +1,16 @@
 #include <stdexcept>
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
-		assertIllegalArgument(guessNumber);
-	}
-	void assertIllegalArgument(const std::string& guessNumber)
-	{
-		if (guessNumber.length() != 3) {
-			throw length_error("Must be three letters");
-		}
-		for (char ch : guessNumber) {
-			if (ch >= '0' || ch <= '9') {
-				throw invalid_argument("Must be number");
-			}
-		}
-		if (isDuplicatedNumber(guessNumber)) {
-			throw invalid_argument("Must not have the same number");
-		}
+	explicit Baseball(const string& question) 
+		: question(question) {
 	}
 	bool isDuplicatedNumber(const std::string& guessNumber)
 	{
@@ -26,4 +18,27 @@ public:
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2];
 	}
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		if (guessNumber.length() != 3) {
+			throw length_error("Must be three letters");
+		}
+		for (char ch : guessNumber) {
+			if (ch >= '0' && ch <= '9') continue;
+				throw invalid_argument("Must be number");
+		}
+		if (isDuplicatedNumber(guessNumber)) {
+			throw invalid_argument("Must not have the same number");
+		}
+	}
+
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+		if (guessNumber == question) {
+			return { true, 3,0 };
+		}
+		return { false, 0, 0 };
+	}
+private:
+	string question;
 };
--- Sample-Test2/baseball_test.cpp
+++ Sample-Test2/baseball_test.cpp
@@ -1,13 +1,12 @@
 #include "pch.h"
 #include "../Baseball/Baseball.cpp"
-using namespace std;
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{"123"};
 	void assertIllegalArgument(string guessNumber) {
 		try {
-			game.guess(string("12s"));
+			game.guess(guessNumber);
 			FAIL();
 		}
 		catch (exception e) {
@@ -20,4 +19,12 @@ TEST_F(BaseballFixture, ThrowExpectionInvalidCases) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
